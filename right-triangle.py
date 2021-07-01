03185815d0479f7cbebc3d9fd6bc748ce4c45936
jianceleste <70678746+jianceleste@users.noreply.github.com>
2021-07-01 15:02:44 +0800

    Create right-triangle.py

---
+++ right-triangle.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def checkIfRightTriangle(num1, num2, num3):
+    a = np.sqrt(num1**2 + num2**2)  # a is the hypotenuse
+    if a == num3: return print('This triangle IS a right triangle!')
+    else: return print('This triangle IS NOT a right triangle!')
+
+
+loop = True
+while loop:
+    try:
+        hypotenuse = int(input('Value of the hypotenuse: '))
+        adjacent = int(input('Value of the adjacent: '))
+        opposite = int(input('Value of the opposite: '))
+
+        checkIfRightTriangle(adjacent, opposite, hypotenuse)
+
+        tryAgain = input('\nWould you like to try again? <y/n> ')
+        if tryAgain.lower() == 'y': loop = True
+        else: loop = False
+
+    except ValueError or TypeError:
+        print('Invalid input! Please try again.\n')
+        loop = True
